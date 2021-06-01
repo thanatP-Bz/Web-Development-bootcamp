77d9968d0d847f51fc130782cdab4c9f86190ffb
Thanat <bztzn512@gmail.com>
2021-05-31 21:53:31 -0400

    testGame

--- JQeury/script.js
+++ JQeury/script.js
@@ -2,3 +2,24 @@ $("h2").addClass("add-color"); //in order to add class
 $("h2").removeClass("slider");
 
 $("button").html("<em>Hey</em>");
+///how to add event listener
+/* $("button").click(function () {
+  $("h1").css("color", "red");
+}); */
+
+/* $(document).click(function () {
+  $("h1").css("color", "green");
+});
+
+$(document).keypress(function (event) {
+  $("h1").text(event.key);
+});
+
+$("h2").on("mouseover", function () {
+  $("h2").css("color", "lightblue");
+});
+ */
+
+$("button").on("click", function () {
+  $("h1").animate({ margin: "20px" });
+});
---
+++ JQeury/simonGame/Simon Game Challenge Starting Files/game.js
@@ -0,0 +1,6 @@
+const nextSequence = function () {
+  const randomNumber = Math.floor(Math.random() * 3) + 1;
+  console.log(randomNumber);
+};
+
+nextSequence();
